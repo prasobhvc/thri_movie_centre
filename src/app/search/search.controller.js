b3f74f8aa74765c66aa526cc0788637f1451570c
prashobh.chandrabose <prashobh.chandrabose.x@visualiq.com>
2016-04-27 13:10:55 +0530

    Search widget added

---
+++ src/app/search/search.controller.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    angular
+            .module('movieCentre')
+            .controller('SearchController', SearchController);
+
+    /** @ngInject */
+    function SearchController() {
+        
+    }
+})();
